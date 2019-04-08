8b8338e75417ba25c97d349c2db5f08ec6854154
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-08 21:32:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
+require "pry"
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+ get '/name' do 
+   "My name is Jennie"
+ end
+ 
+ get '/hometown' do 
+   "My hometown is Stode"
+ end
+ 
+ get '/favorite-song' do 
+   "My favorite song is Natural Woman"
+ end
+  
 end
