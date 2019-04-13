9258816f161b4bf055361d24674794e1eff590ba
Alyssa Brown <alyssa.brown@live.com>
2019-04-13 00:28:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Alyssa"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Denver, Colorado"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Wish You Were Here"
+  end 
 end
