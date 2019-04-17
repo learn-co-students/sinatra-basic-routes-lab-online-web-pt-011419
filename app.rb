9cdbc8a4db14732f8126a172baf13b2fda97fdf2
Kelsey Low <kklowlow@gmail.com>
2019-04-17 03:15:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Twinkle."
+  end
+  
+  get '/hometown' do 
+    "My hometown is in a galaxy far, far, away."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Twinkle Twinkle Little Star."
+  end
+  
 end
